79eba8ebbfc631c473730ff8e28b527817ebe269
BoisonChang <dragoncres@gmail.com>
2022-02-27 18:52:33 +0800

    HOC redux todos

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { useSelector} from 'react-redux'
+import { useSelector, useDispatch} from 'react-redux'
 import { selectorTodos } from './redux/selectors'
 import AddTodo from  './containers/AddTodo'
+import { deleteTodo } from './redux/actions'
 
 function App() {
   // useSelector(store => store.todoState.todos)
   const todos = useSelector(selectorTodos);
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
       <AddTodo />
       <ul>
-      {todos.map(todo => <li> id: {todo.id} ; name: {todo.name}</li>)}
+        {todos.map(todo => <li> id: {todo.id} ; name: {todo.name}
+          <button onClick={() => dispatch(deleteTodo(todo.id))}>delete</button>
+        </li>)}
       </ul>
     </div>
   );
